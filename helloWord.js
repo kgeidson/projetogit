6bf47d0f457cf87598207b049383214ce0b12692
Geidson Silva <geidson.01@gmail.com>
2022-02-11 21:41:43 -0300

    Criado array simples de 1 a 9 tratado com FOR

--- helloWord.js
+++ helloWord.js
@@ -3,8 +3,8 @@ function msg(){
 }
 
 msg();
-//--------------
 
+//--------------
 
 var num1 = 6;
 var num2 = 8;
@@ -12,3 +12,11 @@ var num2 = 8;
 var soma = num1 + num2
 
 console.log(soma);
+
+//--------------
+
+let array = [1,2,3,4,5,6,7,8,9]
+
+for(var i=0; i < array.length; i++){
+    console.log(array[i])
+}
